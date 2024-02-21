e6adf28fa8bdf7f59132387ed1862b4bbef5f876
DaseinDa <64084950+DaseinDa@users.noreply.github.com>
2024-02-21 08:08:00 +0100

    Update vgg.cpp

--- Network_Model/Parallel_Thread/VGG16/VGG16_mnist/vgg.cpp
+++ Network_Model/Parallel_Thread/VGG16/VGG16_mnist/vgg.cpp
@@ -177,8 +177,8 @@ OMP_PARA_INTERNAL																\
 		for (int y = 0; y < GETLENGTH(*weight); ++y)							\
 			wd[x][y] += ((double *)input)[x] * ((double *)outerror)[y];			\
 }
-//((double *)inerror)[x] += ((double *)outerror)[y] * weight[x][y]; 这里是计算input中将要迭代至的上一层的值对Loss的梯度，以便上一层用
-//wd[x][y] += ((double *)input)[x] * ((double *)outerror)[y];	这里是计算当前梯度更新层，要更新的梯度
+//((double *)inerror)[x] += ((double *)outerror)[y] * weight[x][y]; Calculate the gradient of Loss for input layer
+//wd[x][y] += ((double *)input)[x] * ((double *)outerror)[y];	The gradient of Loss for weight, use for weight update
 double relu(double x)
 {
 	return x*(x > 0);
